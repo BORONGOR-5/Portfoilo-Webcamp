c1ffc483bce8b96bc2be7eeaa743437db60aad5c
EC2 Default User <ec2-user@ip-172-31-10-162.ap-northeast-1.compute.internal>
2020-11-13 06:33:29 +0000

    Add_column_Fix_model_entry

---
+++ db/migrate/20201113061122_add_column_to_members.rb
@@ -0,0 +1,13 @@
+class AddColumnToMembers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :members, :name, :string
+    add_column :members, :introduction, :text
+    add_column :members, :profile_image_id, :string
+    add_column :members, :sex, :integer
+    add_column :members, :birthday, :date
+    add_column :members, :best1, :string
+    add_column :members, :best2, :string
+    add_column :members, :best3, :string
+    add_column :members, :is_deleted, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_103935) do
+ActiveRecord::Schema.define(version: 2020_11_13_063109) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,44 @@ ActiveRecord::Schema.define(version: 2020_11_12_103935) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "review_id"
+    t.text "review_comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "entries", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "index_entries_on_member_id"
+    t.index ["room_id"], name: "index_entries_on_room_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "review_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -32,8 +70,73 @@ ActiveRecord::Schema.define(version: 2020_11_12_103935) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "introduction"
+    t.string "profile_image_id"
+    t.integer "sex"
+    t.date "birthday"
+    t.string "best1"
+    t.string "best2"
+    t.string "best3"
+    t.boolean "is_deleted"
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "room_id"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "index_messages_on_member_id"
+    t.index ["room_id"], name: "index_messages_on_room_id"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "title"
+    t.string "image_id"
+    t.text "introduction"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer "visiter_id"
+    t.integer "visited_id"
+    t.integer "review_id"
+    t.integer "comment_id"
+    t.string "action"
+    t.boolean "checked"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["following_id"], name: "index_relationships_on_following_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "movie_id"
+    t.string "title"
+    t.text "Impression"
+    t.float "star"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer "member_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "index_rooms_on_member_id"
   end
 
 end
