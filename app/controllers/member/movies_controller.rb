76676f4df7e8dba8b1360dfd73e29a70dd997efb
EC2 Default User <ec2-user@ip-172-31-10-162.ap-northeast-1.compute.internal>
2020-11-18 18:51:13 +0000

    Add_member_movie

--- app/controllers/member/movies_controller.rb
+++ app/controllers/member/movies_controller.rb
@@ -1,10 +1,20 @@
 class Member::MoviesController < ApplicationController
   def top
+    rank_review_movie_ids = Review.group(:movie_id).order(review: "DESC").limit(3).pluck(:movie_id)
+    @rank_movies = Movie.where(id: rank_review_movie_ids)
+    # .order_as_specified(id: rank_review_movie_ids)
+    @genres = Genre.where(is_active: true)
+    @movies = Movie.all.page(params[:page]).per(3)
   end
 
   def index
+    @genres = Genre.where(is_active: true)
+    @movies = Movie.all.page(params[:page]).per(8)
   end
 
   def show
+    @genres = Genre.where(is_active: true)
+    @movie = Movie.find(params[:id])
+    @reviews = @movie.reviews
   end
 end
