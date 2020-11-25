6ce6c0843ef41dc4b79ec6b4dd0e6cb416752f07
EC2 Default User <ec2-user@ip-172-31-10-162.ap-northeast-1.compute.internal>
2020-11-25 13:56:08 +0000

    Add[管理者の情報を環境変数化]

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,98 +6,98 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-  Member.create!(
-  name: 'グリコ太郎',
-  email: 'aaa@aaa',
-  password: 'aaaaaa',
-  is_deleted: false
-  )
+  # Member.create!(
+  # name: 'グリコ太郎',
+  # email: 'aaa@aaa',
+  # password: 'aaaaaa',
+  # is_deleted: false
+  # )
 
-  Member.create!(
-  name: 'ソニー佐々木',
-  email: 'bbb@bbb',
-  password: 'bbbbbb',
-  is_deleted: false
-  )
+  # Member.create!(
+  # name: 'ソニー佐々木',
+  # email: 'bbb@bbb',
+  # password: 'bbbbbb',
+  # is_deleted: false
+  # )
 
   Admin.create!(
-    email: 'admin@admin.com',
-    password: 'adminadmin'
+    email: ENV['ADMIN_EMAIL'],
+    password: ENV['ADMIN_PASSWORD']
     )
 
-  Genre.create!(
-    id: 1,
-    name: "恋愛/青春",
-    is_active: true
-    )
-  Genre.create!(
-    id: 2,
-    name: "アクション/バトル",
-    is_active: true
-    )
-  Genre.create!(
-    id: 3,
-    name: "ミステリー/サスペンス",
-    is_active: true
-    )
-  Genre.create!(
-    id: 4,
-    name: "SF/ファンタジー",
-    is_active: true
-    )
+  # Genre.create!(
+  #   id: 1,
+  #   name: "恋愛/青春",
+  #   is_active: true
+  #   )
+  # Genre.create!(
+  #   id: 2,
+  #   name: "アクション/バトル",
+  #   is_active: true
+  #   )
+  # Genre.create!(
+  #   id: 3,
+  #   name: "ミステリー/サスペンス",
+  #   is_active: true
+  #   )
+  # Genre.create!(
+  #   id: 4,
+  #   name: "SF/ファンタジー",
+  #   is_active: true
+  #   )
 
-  Movie.create!(
-  genre_id: 2,
-  image_id: '',
-  title: 'マトリックス',
-  introduction: 'マトリックス1作品目です。',
-  )
+  # Movie.create!(
+  # genre_id: 2,
+  # image_id: '',
+  # title: 'マトリックス',
+  # introduction: 'マトリックス1作品目です。',
+  # )
 
-  Movie.create!(
-  genre_id: 3,
-  image_id: '',
-  title: 'シャーロックホームズ',
-  introduction: 'シャーロクホームズです。',
-  )
+  # Movie.create!(
+  # genre_id: 3,
+  # image_id: '',
+  # title: 'シャーロックホームズ',
+  # introduction: 'シャーロクホームズです。',
+  # )
 
-  Movie.create!(
-  genre_id: 1,
-  image_id: '',
-  title: '天気の子',
-  introduction: '「君の名は」で一躍有名になった新海監督の最新作です。',
-  )
+  # Movie.create!(
+  # genre_id: 1,
+  # image_id: '',
+  # title: '天気の子',
+  # introduction: '「君の名は」で一躍有名になった新海監督の最新作です。',
+  # )
 
-  Movie.create!(
-  genre_id: '1',
-  image_id: '',
-  title: '鋼の錬金術師',
-  introduction: '鋼の錬金術師です。',
-  )
+  # Movie.create!(
+  # genre_id: '1',
+  # image_id: '',
+  # title: '鋼の錬金術師',
+  # introduction: '鋼の錬金術師です。',
+  # )
   
-  Review.create!(
-    member_id: "2",
-    movie_id: "3",
-    title: "最高",
-    impression: "実に最高だった"
-    )
+  # Review.create!(
+  #   member_id: "2",
+  #   movie_id: "3",
+  #   title: "最高",
+  #   impression: "実に最高だった"
+  #   )
     
-    Review.create!(
-    member_id: "2",
-    movie_id: "1",
-    title: "まあまあ",
-    impression: "普通"
-    )
+  #   Review.create!(
+  #   member_id: "2",
+  #   movie_id: "1",
+  #   title: "まあまあ",
+  #   impression: "普通"
+  #   )
     
-    Review.create!(
-    member_id: "1",
-    movie_id: "3",
-    title: "うーん",
-    impression: "微妙だった"
-    )
+  #   Review.create!(
+  #   member_id: "1",
+  #   movie_id: "3",
+  #   title: "うーん",
+  #   impression: "微妙だった"
+  #   )
     
-    Review.create!(
-    member_id: "2",
-    movie_id: "2",
-    title: "悪くはない",
-    impression: "普通"
-    )
+  #   Review.create!(
+  #   member_id: "2",
+  #   movie_id: "2",
+  #   title: "悪くはない",
+  #   impression: "普通"
+  #   )
